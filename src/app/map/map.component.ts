aa917ac5c6dcb5c6cfda64cdcaf830c580c57e08
MarioK35 <mariops3gow3@gmail.com>
2020-11-12 10:05:31 -0600

    Mapa

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -16,28 +16,20 @@ private est;
    this.initMap();
  }
 private initMap():void{
-this.map.createPane('labels');
-this.map.getPane('labels').style.zIndex = 650;
-this.map.getPane('labels').style.pointerEvents = 'none';
+
+  
+
+var countries = L.geoJson(countries);
+
 this.map = L.map('map').setView([25, -90],6);
-this.est = 'estados-municipios.json'
+
+
 const tiles = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png', {
 	maxZoom: 20,
 	attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
 });
 
-var positron = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png', {
-        attribution: '©OpenStreetMap, ©CartoDB'
-});
-
-var positronLabels = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_only_labels/{z}/{x}/{y}.png', {
-        attribution: '©OpenStreetMap, ©CartoDB',
-        pane: 'labels'
-});
-
-
-positronLabels.addTo(this.map)
-positron.addTo(this.map)
 tiles.addTo(this.map)
+countries.addTo(this.map)
 }
 }
